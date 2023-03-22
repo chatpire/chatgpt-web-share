9a4c50e6e137b0fbb7d2c6467d80f69335f40d24
moeakwak <moeakwak@gmail.com>
2023-03-22 16:32:23 +0800

    fix: vite base path; error page margin

--- frontend/config/vite.config.base.ts
+++ frontend/config/vite.config.base.ts
@@ -10,7 +10,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/",
   plugins: [
     vue(),
     UnoCSS({
--- frontend/src/views/error/404.vue
+++ frontend/src/views/error/404.vue
@@ -1,6 +1,6 @@
 <template>
-  <!-- 403 error page -->
-  <div class="w-full h-full lg:w-screen-lg mx-auto h-screen flex flex-col">
+  <!-- 404 error page -->
+  <div class="w-full h-full lg:w-screen-lg mx-auto h-screen flex flex-col mt-3">
     <div class="flex-1 flex justify-center items-center">
       <n-card class="h-full mb-6 flex justify-center items-center">
         <div class="text-2xl text-gray-500">
