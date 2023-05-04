6a9bbdf6b0f9b7e3446f59e00b6a1fb4b5570f4a
moeakwak <moeakwak@gmail.com>
2023-05-04 19:47:28 +0800

    refactor: sqlalchemy custom types (add GUID type)

---
+++ backend/api/database/custom_types/guid.py
@@ -0,0 +1,40 @@
+from sqlalchemy.types import TypeDecorator, CHAR
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+
+class GUID(TypeDecorator):
+    """Platform-independent GUID type.
+    https://docs.sqlalchemy.org/en/20/core/custom_types.html#backend-agnostic-guid-type
+
+    Uses PostgreSQL's UUID type, otherwise uses CHAR(32), storing as stringified hex values.
+    """
+
+    impl = CHAR
+    cache_ok = True
+
+    def load_dialect_impl(self, dialect):
+        if dialect.name == "postgresql":
+            return dialect.type_descriptor(UUID())
+        else:
+            return dialect.type_descriptor(CHAR(32))
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return value
+        elif dialect.name == "postgresql":
+            return str(value)
+        else:
+            if not isinstance(value, uuid.UUID):
+                return "%.32x" % uuid.UUID(value).int
+            else:
+                # hexstring
+                return "%.32x" % value.int
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return value
+        else:
+            if not isinstance(value, uuid.UUID):
+                value = uuid.UUID(value)
+            return value
