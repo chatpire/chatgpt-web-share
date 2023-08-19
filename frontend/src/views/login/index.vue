5126370ec094928b5f56d2ea4838bad760c5b8b7
spammenotinoz <spammenotinoz@gmail.com>
2023-08-19 23:31:04 +1000

    Update index.vue

--- frontend/src/views/login/index.vue
+++ frontend/src/views/login/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <!-- ... (rest of the template) ... -->
+  <div class="login-container">
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <div class="input-group">
+        <label for="username">Username:</label>
+        <input type="text" id="username" v-model="formValue.username" required />
+      </div>
+      <div class="input-group">
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="formValue.password" required />
+      </div>
+      <button type="submit" :disabled="loading">Login</button>
+    </form>
+    <div class="paypal-button">
+      <!-- You can integrate the PayPal button here -->
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -49,3 +65,43 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.login-container {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.input-group {
+  margin-bottom: 15px;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+input {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+button {
+  padding: 10px 15px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:disabled {
+  background-color: #ccc;
+  cursor: not-allowed;
+}
+</style>
