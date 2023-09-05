ed95222d71bbdf9892e9b13e9b4427d4332c5bed
spammenotinoz <spammenotinoz@gmail.com>
2023-09-05 02:10:06 +0000

    Update PageHeader.vue

--- frontend/src/components/PageHeader.vue
+++ frontend/src/components/PageHeader.vue
@@ -8,12 +8,8 @@
           </div>
           <div class="hidden sm:block">
             <a class="h-full inline-block flex" href="https://UltimateAI.org" target="_blank">
-              <img
-              src="https://UltimateAI.org/UltimateAI.jpg"
-                alt="AltimateAI"
-                :style="{ color: appStore.theme == 'dark' ? 'white' : 'black' }"
-                />
-            </a>        
+              <n-icon :color="appStore.theme == 'dark' ? 'white' : 'black'" :component="LogoGithub" />
+            </a>
           </div>
         </n-space>
       </template>
