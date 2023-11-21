1788c6f11e5cc0b4a06fa784b4fd0b7ddc517132
moeakwak <moeakwak@gmail.com>
2023-11-21 13:09:25 +0800

    fix: math block delimiter line wrap

--- frontend/src/utils/chat.ts
+++ frontend/src/utils/chat.ts
@@ -278,9 +278,9 @@ export function replaceMathDelimiters(input: string) {
         const endMarker = isInline ? '\\)' : '\\]';
         const endPos = input.indexOf(endMarker, pos + 2);
         if (endPos >= 0) {
-          output += isInline ? '$' : '$$';
+          output += isInline ? '$' : '$$\n';
           output += input.substring(pos + 2, endPos).trim();
-          output += isInline ? '$' : '$$';
+          output += isInline ? '$' : '\n$$';
           pos = endPos + endMarker.length;
           continue;
         }
